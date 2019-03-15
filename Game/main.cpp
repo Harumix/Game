4723a9a2457e550d7f3ba333d2322a5a5de80a05
Artur Jackowiak <artur.jackowiak@op.pl>
2019-03-15 15:51:05 +0100

    Dalsze testrowanie

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
+#include "ResourcePath.hpp""
 
 using std::cout;
 using std::cin;
@@ -9,7 +10,7 @@ using std::endl;
 int main()
 {
 	// Tworzenie okna 
-	sf::Window window(sf::VideoMode(800,600),"Game");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Game");
 	// Pozycja startowa okna
 	window.setPosition(sf::Vector2i(100, 50));
 	// Rozmiar okna
@@ -17,34 +18,33 @@ int main()
 	// Nazwa okna
 	window.setTitle("Game");
 
-	/*
-
 	// Set the Icon
 	sf::Image icon;
-	if (!icon.loadFromFile("icon.jpg")) {
+	if (!icon.loadFromFile("../External/Graphics/Lion.jpg")) {
 		return EXIT_FAILURE;
 	}
 	window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
 	// Load a sprite to display
-	sf::Texture texture;
-	if (!texture.loadFromFile("texture.jpg")) {
+	sf::Texture lionTexture;
+	if (!lionTexture.loadFromFile("../External/Graphics/Lion.jpg")) {
 		return EXIT_FAILURE;
 	}
-	sf::Sprite sprite(texture);
+	sf::Sprite lionSprite(lionTexture);
+
+	/*
 
 	// Create a graphical text to display
 	sf::Font font;
 	if (!font.loadFromFile("font.ttf")) {
-		return EXIT_FAILURE;
+		//return EXIT_FAILURE;
 	}
 	sf::Text text("Hello World", font, 50);
-	text.setColor(sf::Color::Black);
 
 	// Load a music to play
 	sf::Music music;
 	if (!music.openFromFile("music.mp3")) {
-		return EXIT_FAILURE;
+		//return EXIT_FAILURE;
 	}
 
 	// Play music
@@ -71,10 +71,12 @@ int main()
 		}
 		
 		// Clear screen
-		//window.clear();
+		window.clear();
+
 		
+	
 		// Draw the sprite
-		//window.draw(sprite);
+		window.draw(lionSprite);
 
 		// Draw the string
 		//window.draw(text);
